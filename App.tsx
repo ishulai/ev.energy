3b24c98529287ee1da71fdd8fa0f6bd24e142394
Thomas Lai <thomas990428@me.com>
2022-11-15 22:42:45 -0800

    Implement location retrieval

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {useState, useEffect} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+import * as Location from 'expo-location';
+
+import * as OCMConfig from './OCMConfig.json';
 
 export default function App() {
+  const [chargers, setChargers] = useState<Array<Object> | null>(null);
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [error, setError] = useState<String | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const permissions = await Location.requestForegroundPermissionsAsync();
+      if (permissions.status !== 'granted') {
+        setError('Permission to access location was denied');
+        return;
+      }
+      const location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
